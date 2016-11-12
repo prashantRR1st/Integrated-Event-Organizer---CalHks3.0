9ac6c6e8e2c6a272ccc12aed4d6ac0579452af6c
Fadi Francis <fadifrancis@Fadis-MacBook-Pro.local>
2016-11-12 11:46:57 -0800

    added uber sidebar tab

--- native-starter-kit-master/js/components/sideBar/index.js
+++ native-starter-kit-master/js/components/sideBar/index.js
@@ -30,6 +30,9 @@ class SideBar extends Component {
           <ListItem button onPress={() => this.navigateTo('blankPage')} >
             <Text>Blank Page</Text>
           </ListItem>
+          <ListItem button onPress={() => this.navigateTo('Uber')} >
+            <Text>Uber</Text>
+          </ListItem>
         </List>
       </Content>
     );
